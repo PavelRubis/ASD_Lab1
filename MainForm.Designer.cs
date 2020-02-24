34bce3eb0da8e12d6fd2f9d305bad737072f4466
Pavel Rubis <rubisiay@gmail.com>
2020-02-24 15:07:43 +0300

    Коммит №1. Лаба №1 по АСП Петракова.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Lab1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TrapezesMethodCheckBox = new System.Windows.Forms.CheckBox();
+            this.ImplicitEMethodCheckBox = new System.Windows.Forms.CheckBox();
+            this.ExplicitEMethodCheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CapacityTextBox = new System.Windows.Forms.TextBox();
+            this.ResistTextBox = new System.Windows.Forms.TextBox();
+            this.VoltageTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.HTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.CapacityTextBox);
+            this.panel1.Controls.Add(this.ResistTextBox);
+            this.panel1.Controls.Add(this.VoltageTextBox);
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 446);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 115);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.HTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(480, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(175, 92);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Параметр h в долях от RC:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TrapezesMethodCheckBox);
+            this.groupBox1.Controls.Add(this.ImplicitEMethodCheckBox);
+            this.groupBox1.Controls.Add(this.ExplicitEMethodCheckBox);
+            this.groupBox1.Location = new System.Drawing.Point(274, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 92);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Итерационные методы расчета:";
+            // 
+            // TrapezesMethodCheckBox
+            // 
+            this.TrapezesMethodCheckBox.AutoSize = true;
+            this.TrapezesMethodCheckBox.Checked = true;
+            this.TrapezesMethodCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.TrapezesMethodCheckBox.Location = new System.Drawing.Point(17, 65);
+            this.TrapezesMethodCheckBox.Name = "TrapezesMethodCheckBox";
+            this.TrapezesMethodCheckBox.Size = new System.Drawing.Size(79, 19);
+            this.TrapezesMethodCheckBox.TabIndex = 2;
+            this.TrapezesMethodCheckBox.Text = "Трапеций";
+            this.TrapezesMethodCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ImplicitEMethodCheckBox
+            // 
+            this.ImplicitEMethodCheckBox.AutoSize = true;
+            this.ImplicitEMethodCheckBox.Checked = true;
+            this.ImplicitEMethodCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ImplicitEMethodCheckBox.Location = new System.Drawing.Point(17, 40);
+            this.ImplicitEMethodCheckBox.Name = "ImplicitEMethodCheckBox";
+            this.ImplicitEMethodCheckBox.Size = new System.Drawing.Size(119, 19);
+            this.ImplicitEMethodCheckBox.TabIndex = 1;
+            this.ImplicitEMethodCheckBox.Text = "Неявный Эйлера";
+            this.ImplicitEMethodCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ExplicitEMethodCheckBox
+            // 
+            this.ExplicitEMethodCheckBox.AutoSize = true;
+            this.ExplicitEMethodCheckBox.Checked = true;
+            this.ExplicitEMethodCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ExplicitEMethodCheckBox.Location = new System.Drawing.Point(17, 15);
+            this.ExplicitEMethodCheckBox.Name = "ExplicitEMethodCheckBox";
+            this.ExplicitEMethodCheckBox.Size = new System.Drawing.Size(105, 19);
+            this.ExplicitEMethodCheckBox.TabIndex = 0;
+            this.ExplicitEMethodCheckBox.Text = "Явный Эйлера";
+            this.ExplicitEMethodCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "U0, В";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Параметры\r\nмоделирования:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "R, Ом";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "C, нФ";
+            // 
+            // CapacityTextBox
+            // 
+            this.CapacityTextBox.Location = new System.Drawing.Point(144, 80);
+            this.CapacityTextBox.Name = "CapacityTextBox";
+            this.CapacityTextBox.Size = new System.Drawing.Size(100, 23);
+            this.CapacityTextBox.TabIndex = 3;
+            this.CapacityTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            this.CapacityTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // ResistTextBox
+            // 
+            this.ResistTextBox.Location = new System.Drawing.Point(144, 45);
+            this.ResistTextBox.Name = "ResistTextBox";
+            this.ResistTextBox.Size = new System.Drawing.Size(100, 23);
+            this.ResistTextBox.TabIndex = 2;
+            this.ResistTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            this.ResistTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // VoltageTextBox
+            // 
+            this.VoltageTextBox.Location = new System.Drawing.Point(144, 10);
+            this.VoltageTextBox.Name = "VoltageTextBox";
+            this.VoltageTextBox.Size = new System.Drawing.Size(100, 23);
+            this.VoltageTextBox.TabIndex = 1;
+            this.VoltageTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            this.VoltageTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(661, 44);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(111, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Смоделировать";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Chart
+            // 
+            this.Chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea5.AxisX.Title = "t, c";
+            chartArea5.AxisY.Title = "U, В";
+            chartArea5.Name = "Area";
+            this.Chart.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.Chart.Legends.Add(legend5);
+            this.Chart.Location = new System.Drawing.Point(0, 2);
+            this.Chart.Name = "Chart";
+            this.Chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            this.Chart.Size = new System.Drawing.Size(784, 440);
+            this.Chart.TabIndex = 1;
+            this.Chart.Text = "chart1";
+            this.Chart.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Chart_MouseWheelZoom);
+            // 
+            // HTextBox
+            // 
+            this.HTextBox.Location = new System.Drawing.Point(35, 36);
+            this.HTextBox.Name = "HTextBox";
+            this.HTextBox.Size = new System.Drawing.Size(100, 23);
+            this.HTextBox.TabIndex = 3;
+            this.HTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            this.HTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Chart);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Лаб. №1 АСП: Моделирование разрядки конденсатора через резистор. МИЭТ, 2020";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox CapacityTextBox;
+        private System.Windows.Forms.TextBox ResistTextBox;
+        private System.Windows.Forms.TextBox VoltageTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox TrapezesMethodCheckBox;
+        private System.Windows.Forms.CheckBox ImplicitEMethodCheckBox;
+        private System.Windows.Forms.CheckBox ExplicitEMethodCheckBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox HTextBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace Lab1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            bool error = default;
+            string errorStr = "Ошибки в полях: ";
+
+            if (!Regex.IsMatch(VoltageTextBox.Text, @"^\d{1},?(?:(?<=[,])\d+|$)(?<=\d)$"))
+            {
+                error = true;
+                errorStr += "Величина напряжения должна быть в диапазоне от 0 до 10 В; ";
+            }
+
+            if (!Regex.IsMatch(ResistTextBox.Text, @"^\d{1,2},?(?:(?<=[,])\d+|$)(?<=\d)$"))
+            {
+                error = true;
+                errorStr += "Величина сопротивления должна быть в диапазоне от 0 до 100 Ом; ";
+            }
+
+            if (!Regex.IsMatch(CapacityTextBox.Text, @"^\d{1,2},?(?:(?<=[,])\d+|$)(?<=\d)$"))
+            {
+                error = true;
+                errorStr += "Величина емкости должна быть в диапазоне от 0 до 100 нФ; ";
+            }
+
+            if (!Regex.IsMatch(HTextBox.Text, @"^\d{1},?(?:(?<=[,])\d+|$)(?<=\d)$"))
+            {
+                error = true;
+                errorStr += "Величина параметра h должна быть в диапазоне от 0 до 10 RC; ";
+            }
+
+            if (error)
+            {
+                MessageBox.Show(
+                    Regex.Replace(errorStr,@"; $",""),
+                    "Ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1);
+                return;
+            }
+            Chart.Series.Clear();
+            try
+            {
+                if (ExplicitEMethodCheckBox.Checked)
+                {
+                    Series ExplicitSeries = Chart.Series.Add("Явный метод Эйлера");
+                    ExplicitSeries.ChartType = SeriesChartType.Line;
+                    ExplicitSeries.MarkerStyle = MarkerStyle.Circle;
+                    ExplicitSeries.MarkerSize = 11;
+                    ExplicitSeries.BorderWidth = 3;
+
+                    ExplicitEMethod(Double.Parse(VoltageTextBox.Text), Double.Parse(ResistTextBox.Text), Double.Parse(CapacityTextBox.Text), Double.Parse(HTextBox.Text), ExplicitSeries);
+                }
+
+                if (ImplicitEMethodCheckBox.Checked)
+                {
+                    Series ImplicitSeries = Chart.Series.Add("Неявный метод Эйлера");
+                    ImplicitSeries.ChartType = SeriesChartType.Line;
+                    ImplicitSeries.MarkerStyle = MarkerStyle.Square;
+                    ImplicitSeries.MarkerSize = 11;
+                    ImplicitSeries.BorderWidth = 3;
+
+                    ImplicitEMethod(Double.Parse(VoltageTextBox.Text), Double.Parse(ResistTextBox.Text), Double.Parse(CapacityTextBox.Text), Double.Parse(HTextBox.Text), ImplicitSeries);
+                }
+
+                if (TrapezesMethodCheckBox.Checked)
+                {
+                    Series TrapezesSeries = Chart.Series.Add("Метод трапеций");
+                    TrapezesSeries.ChartType = SeriesChartType.Line;
+                    TrapezesSeries.MarkerStyle = MarkerStyle.Diamond;
+                    TrapezesSeries.MarkerSize = 11;
+                    TrapezesSeries.BorderWidth = 3;
+
+                    TrapezesMethod(Double.Parse(VoltageTextBox.Text), Double.Parse(ResistTextBox.Text), Double.Parse(CapacityTextBox.Text), Double.Parse(HTextBox.Text), TrapezesSeries);
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                    "Ошибка преобразования данных. " + ex.Message,
+                    "Ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Ошибка преобразования данных. "+ ex.Message,
+                    "Ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                return;
+            }
+        }
+
+        //Явный метод Эйлера
+        public void ExplicitEMethod(double U0, double R, double C, double h, Series plot)
+        {
+            C *= Math.Pow(10, -9);
+            h *= (R * C);
+
+            double t = default;
+            double phi = U0;
+            double u = U0;
+            int n = 0;
+
+            while (t < (R * C))
+            {
+                plot.Points.AddXY(t, phi);
+                phi = u * (Math.Pow((1 - (h / (R * C))), n + 1));
+                t += (R * C) / 20;
+                u = U0 * Math.Pow(Math.E, -t / (R * C));
+                n++;
+            }
+        }
+
+        //Неявный метод Эйлера
+        public void ImplicitEMethod(double U0, double R, double C, double h, Series plot)
+        {
+            C *= Math.Pow(10, -9);
+            h *= (R * C);
+
+            double t = default;
+            double phi = U0;
+            double u = U0;
+            int n = 0;
+
+            while (t < (R * C))
+            {
+                plot.Points.AddXY(t, phi);
+                phi = u / (Math.Pow((1 + (h / (R * C))),n+1));
+                t += (R * C) / 20;
+                u = U0 * Math.Pow(Math.E, -t / (R * C));
+                n++;
+            }
+        }
+
+        //Метод трапеций
+        public void TrapezesMethod(double U0, double R, double C, double h, Series plot)
+        {
+            C *= Math.Pow(10, -9);
+            h *= (R * C);
+
+            double t = default;
+            double phi = U0;
+            double u = U0;
+            int n = 0;
+
+            while (t < (R * C))
+            {
+                plot.Points.AddXY(t, phi);
+                phi = (u * (Math.Pow((1 - (h / (R * C))), n + 1))) + (u / (Math.Pow((1 + (h / (R * C))), n + 1)));
+                phi /= 2;
+                t += (R * C) / 20;
+                u = U0 * Math.Pow(Math.E, -t / (R * C));
+                n++;
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.A:
+                        if (sender != null)
+                            (sender as TextBox).SelectAll();
+                        break;
+                    case Keys.C:
+                        if (sender != null)
+                            (sender as TextBox).Copy();
+                        break;
+                    case Keys.X:
+                        if (sender != null)
+                            (sender as TextBox).Cut();
+                        break;
+                    case Keys.V:
+                        if (sender != null)
+                            (sender as TextBox).Paste();
+                        break;
+                }
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((!Char.IsDigit(number) && e.KeyChar != 8 && e.KeyChar != ',') || (e.KeyChar == ',' && (sender as TextBox).Text.IndexOf(",") != -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch((sender as TextBox).Text, @"\D"))
+            {
+                if (Regex.Match((sender as TextBox).Text, @"\D").Value != ",")
+                {
+                    (sender as TextBox).Text = "";
+                }
+            }
+        }
+
+        private void Chart_MouseWheelZoom(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta < 0)
+                {
+                    Chart.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                    Chart.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+                }
+                if (e.Delta > 0)
+                {
+                    double xMin = Chart.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = Chart.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    double yMin = Chart.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    double yMax = Chart.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+                    double posXStart = Chart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 3;
+                    double posXFinish = Chart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 3;
+                    double posYStart = Chart.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 3;
+                    double posYFinish = Chart.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 3;
+                    Chart.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    Chart.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                return;
+            }
+        }
+    }
+}
